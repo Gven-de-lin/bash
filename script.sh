3987e6c43bf14c1493bb2d11aafb5fffd49f6abb
Gven-de-lin <novik.anfiso4ka@gmail.com>
2022-06-22 13:30:03 +0300

    Add homework of bash

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd foldername
+mkdir foldername_1 foldername_2 foldername_3
+cd foldername_1
+touch foldername1.txt foldername2.txt foldername3.txt foldername4.json foldername5.json
+mkdir foldername_1.1 foldername_1.2 foldername_1.3
+ls -la
+mv foldername1.txt foldername2.txt foldername_1.1
+
